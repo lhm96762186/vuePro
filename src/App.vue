764d2b2759fc3ec9f039dc28dd1fea50f01df755
lemon <2413029730@qq.com>
2018-06-29 18:37:15 +0800

    first modify

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+export default {
+  name: 'App',
+  data(){
+  	return {
+  		
+  	}
+  },
+  components:{
+  	Home
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="wrap">
+		home页{{msg}}
+	</div>
+</template>
+
+<script>
+export default {
+	name:'home',
+	data(){
+		return{
+			msg:'hello'
+		}
+	}
+}
+</script>
+
+<style>
+.wrap{
+	color: red;
+}
+</style>
